5bb5785556076a70698b1312e2224093143374e2
Frank Dai <zhoulin.dai@gmail.com>
2017-03-07 22:48:50 +0800

    Fix rack_env issue

--- bin/run_company_crawler.sh
+++ bin/run_company_crawler.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --link threebandb -v ~/projects/threeban:/code --rm ruby:2.2.0 sh -c "cd /code; bundle install; ruby /code/lib/company_crawler.rb"
+docker run -it --link threebandb -v ~/projects/threeban:/code --rm ruby:2.2.0 sh -c "cd /code; bundle install; RACK_ENV=production ruby /code/lib/company_crawler.rb"
