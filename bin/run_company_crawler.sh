548a7356af353c7b846b0d58d153112586188cfc
Frank Dai <zhoulin.dai@gmail.com>
2017-03-09 21:37:36 +0800

    fix shell wrapper

--- bin/run_company_crawler.sh
+++ bin/run_company_crawler.sh
@@ -5,7 +5,7 @@ DBNAME=company_crawler
 RUNNING=`docker inspect --format="{{ .State.Running }}" $DBNAME`
 if [ $? -eq 1 ]; then
     echo "Container $DBNAME does not exist. Creating..."
-    docker create -it --name $DBNAME -v $ROOT/..:/code ruby:2.2.0
+    docker create -it --link threebandb --name $DBNAME -v $ROOT/..:/code ruby:2.2.0
 fi
 
 RUNNING=`docker inspect --format="{{ .State.Running }}" $DBNAME`
