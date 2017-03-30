3cdee74a3fd8cca06ecf1a1e9413a092a81aa942
Frank Dai <zhoulin.dai@gmail.com>
2017-03-30 22:57:49 +0800

    Update dev

--- lib/disclosure_crawler.rb
+++ lib/disclosure_crawler.rb
@@ -3,6 +3,7 @@
 require_relative "http_helper"
 require "json"
 require "time"
+
 require_relative "models/disclosure"
 require_relative "utils"
 require "pdf-reader"
@@ -12,7 +13,6 @@ require "concurrent"
 module ThreeBan
   class DisclosureCrawler
     NEEQ_HOST = "http://www.neeq.com.cn"
-    
     def initialize()
     end
 
@@ -37,8 +37,8 @@ module ThreeBan
           disclosure_list = resp_json[0]["listInfo"]["content"]
           disclosure_list.each {|disclosure|
             if disclosure["disclosureCode"] == last_code
-              is_done = true
-              break
+            is_done = true
+            break
             end
             save_disclosure(disclosure)
           }
@@ -49,10 +49,10 @@ module ThreeBan
         start_date = Date.strptime(end_date_str, "%Y-%m-%d")
       end
     end
-    
+
     def crawl_disclosure_content
-      disclosures = ::Disclosures.find({:content => {"$exists" => false}}, \
-        {:sort => {:publishTime => 1}, :limit => 20})
+      disclosures = Disclosure.where({:content => {"$exists" => false}}, \
+      {:sort => {:publishTime => 1}, :limit => 20})
       httpclient = HTTPHelper.new(NEEQ_HOST)
       count = 3
       pool = Concurrent::FixedThreadPool.new(count)
@@ -64,60 +64,60 @@ module ThreeBan
       pool.shutdown
       pool.wait_for_termination
     end
-    
+
     def get_last_date
-      rec = ::Disclosures.last(nil, :sort => {:publishTime => 1})
-      rec[:publishDate]
+      rec = Disclosure.order_by(:publishTime => 'desc').first
+      rec.publishDate rescue nil
     end
-    
+
     def get_last_disclosure_code
-      rec = ::Disclosures.last(nil, :sort => {:publishTime => 1})
-      rec[:disclosureCode]
+      rec = Disclosure.order_by(:publishTime => 'desc').first
+      rec.disclosureCode rescue nil
     end
-    
-    def save_disclosure_content(disc)      
+
+    def save_disclosure_content(disc)
       begin
-          content = nil
-          if disc[:filePath].end_with?(".pdf")
-            io     = open("#{NEEQ_HOST}#{disc[:filePath]}")
-            reader = PDF::Reader.new(io)
-            content = ""
-            reader.pages.each { |page|
-              page.text.split("\n").each {|line|
-                next if line.length == 0
-                if line.start_with?(" ")
-                  content += "\n#{line}"
-                else
-                  content += line 
-                end
-              }
-            }
-          elsif disc[:filePath].end_with?(".txt")
-              resp = httpclient.get(disc[:filePath])
-              if resp.code == '200'
-                content = resp.body 
+        content = nil
+        if disc[:filePath].end_with?(".pdf")
+          io     = open("#{NEEQ_HOST}#{disc[:filePath]}")
+          reader = PDF::Reader.new(io)
+          content = ""
+          reader.pages.each { |page|
+            page.text.split("\n").each {|line|
+              next if line.length == 0
+              if line.start_with?(" ")
+                content += "\n#{line}"
               else
-                puts "Failed to get content for disclosure #{disc[:filePath]}, code: #{resp.code}"
-                return
+              content += line
               end
+            }
+          }
+        elsif disc[:filePath].end_with?(".txt")
+          resp = httpclient.get(disc[:filePath])
+          if resp.code == '200'
+          content = resp.body
           else
-              puts "Unsupported suffix #{disc[:filePath]}"
-              return
+            puts "Failed to get content for disclosure #{disc[:filePath]}, code: #{resp.code}"
+          return
           end
-          
-          puts "Saving content for disclosureCode #{disc[:disclosureCode]} on #{disc[:publishTime]}"
-          ::Disclosures.update({:disclosureCode => disc[:disclosureCode]}, {:content => content, :hasContent => true})
-        rescue Exception => e
-          puts "Failed to get content for disclosure #{disc[:filePath]}, #{e.class.name}"
-          puts e.backtrace
-          ::Disclosures.update({:disclosureCode => disc[:disclosureCode]}, {:content => "error", :hasContent => true})
+        else
+          puts "Unsupported suffix #{disc[:filePath]}"
+        return
         end
+
+        puts "Saving content for disclosureCode #{disc[:disclosureCode]} on #{disc[:publishTime]}"
+        ::Disclosure.update({:disclosureCode => disc[:disclosureCode]}, {:content => content, :hasContent => true})
+      rescue Exception => e
+        puts "Failed to get content for disclosure #{disc[:filePath]}, #{e.class.name}"
+        puts e.backtrace
+        Disclosure.update({:disclosureCode => disc[:disclosureCode]}, {:content => "error", :hasContent => true})
+      end
     end
-    
+
     def save_disclosure(disclosure)
       code = disclosure["companyCd"]
       name = disclosure["companyName"].gsub(" ", "")
-      filePath = disclosure["destFilePath"]
+      fileURL = disclosure["destFilePath"]
       disclosureCode = disclosure["disclosureCode"]
       disclosureTitle = disclosure["disclosureTitle"]
       type = disclosure["disclosureType"]
@@ -131,17 +131,33 @@ module ThreeBan
       when "9510"
         "首次信息披露"
       else
-        "Other-#{type}"
+      "Other-#{type}"
       end
       publishDate = disclosure["publishDate"]
       str = disclosure["upDate"]["time"]
       publishTime = Time.at(str.to_i/1000)
-      ::Disclosures.upsert({:code => code, :disclosureCode => disclosureCode},
-        {:code => code, :name => name, :filePath => filePath, 
-         :disclosureCode => disclosureCode, :disclosureTitle => disclosureTitle,
-         :disclosureType => disclosureType, :publishDate => publishDate,
-         :publishTime => publishTime, :updated_at => Time.now}
+      Disclosure.create(
+        :code => code, :name => name, :fileURL => fileURL,
+        :disclosureCode => disclosureCode, :disclosureTitle => disclosureTitle,
+        :disclosureType => disclosureType, :publishDate => publishDate,
+        :publishTime => publishTime
       )
+    end
+
+    def download_disclosure(code, url)
+      filename = url.split("/").last
+      data_root = "/data"
+      folder_path = data_root + "/" + code.split("").join("/")
+      file_path = folder_path + "/" + filename
+      begin
+        FileUtils.mkdir_p(folder_path)
+        open(file_path, "w") do |file|
+          file << open(url).read
+        end
+        file_path
+      rescue Exception => e
+        puts e.backtrace 
+      end
     end
   end
 end
@@ -149,8 +165,8 @@ end
 if __FILE__ == $0
   crawler = ThreeBan::DisclosureCrawler.new
   puts "Start: #{Time.now}"
-  #crawler.crawl_disclosure
-  crawler.crawl_disclosure_content
+  crawler.crawl_disclosure
+  #crawler.crawl_disclosure_content
   puts "End: #{Time.now}"
-  
+
 end
--- lib/models/disclosure.rb
+++ lib/models/disclosure.rb
@@ -5,11 +5,14 @@ class Disclosure
 
   field :name, :type => String
   field :code, :type => String
-  field :filePath, :type => String
+  field :fileURL, :type => String
   field :disclosureCode, :type => String
   field :disclosureTitle, :type => String
   field :disclosureType, :type => String
   field :publishDate, :type => Date
   field :publishTime, :type => Time
-  field :crawledDate, :type => Time
+  field :isDownloaded, :type => Boolean, :default => false
+  field :filePath, :type => String
+  field :isContentParse, :type => Boolean, :default => false
+  field :txtPath, :type => String
 end
