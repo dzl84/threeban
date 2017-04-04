80a1b836ceea5c340ad389628415683d58a6274c
fdai <fdai@HomeServer.lan>
2017-04-04 17:56:26 +0800

    update wrapper

--- bin/run_disclosure_crawler.sh
+++ bin/run_disclosure_crawler.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
 ROOT=`dirname $(readlink -f "$0")`
-DBNAME=disclosure_crawler
-RUNNING=`docker inspect --format="{{ .State.Running }}" $DBNAME`
+NAME=disclosure_crawler
+RUNNING=`docker inspect --format="{{ .State.Running }}" $NAME`
 if [ $? -eq 1 ]; then
-    echo "Container $DBNAME does not exist. Creating..."
-    docker create -it --link threebandb --name $DBNAME -v $ROOT/..:/code -v $ROOT/../logs:/logs -v $ROOT/../data/disclosures:/data/disclosures ruby:2.3.0
+    echo "Container $NAME does not exist. Creating..."
+    docker create -it --link threebandb --name $NAME -v $ROOT/..:/code -v $ROOT/../logs:/logs -v $ROOT/../data/disclosures:/data/disclosures ruby:2.3.0
 fi
 
-RUNNING=`docker inspect --format="{{ .State.Running }}" $DBNAME`
+RUNNING=`docker inspect --format="{{ .State.Running }}" $NAME`
 if [ "$RUNNING" == "false" ]; then
-    echo "Container $DBNAME is not running"
-    docker start $DBNAME
+    echo "Container $NAME is not running"
+    docker start $NAME
+else
+    echo "Container $NAME is already running, exiting"
+    exit 0
 fi
 
-docker exec -i $DBNAME sh -c "cd /code; bundle install"
+docker exec -i $NAME sh -c "cd /code; bundle install"
 
-docker exec -i $DBNAME sh -c "RACK_ENV=production ruby /code/lib/disclosure_crawler.rb --action crawl-list >> /logs/disclosure_crawler.log 2>&1"
+docker exec -i $NAME sh -c "RACK_ENV=production ruby /code/lib/disclosure_crawler.rb --action crawl-list >> /logs/disclosure_crawler.log 2>&1"
+
+docker stop $NAME
