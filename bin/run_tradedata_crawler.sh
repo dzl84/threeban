cf08e25cfbd22ff8fb7fec842581765982f2b0c6
Frank Dai <zhoulin.dai@gmail.com>
2017-04-06 22:13:33 +0800

    update wrapper

--- bin/run_tradedata_crawler.sh
+++ bin/run_tradedata_crawler.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
 ROOT=`dirname $(readlink -f "$0")`
-DBNAME=tradedata_crawler
-RUNNING=`docker inspect --format="{{ .State.Running }}" $DBNAME`
+NAME=tradedata_crawler
+RUNNING=`docker inspect --format="{{ .State.Running }}" $NAME`
 if [ $? -eq 1 ]; then
-    echo "Container $DBNAME does not exist. Creating..."
-    docker create -it --link threebandb --name $DBNAME -v $ROOT/..:/code -v $ROOT/../logs:/logs ruby:2.2.0
+    echo "Container $NAME does not exist. Creating..."
+    docker create -it --link threebandb --name $NAME -v $ROOT/..:/code -v $ROOT/../logs:/logs ruby:2.3.4
 fi
 
-RUNNING=`docker inspect --format="{{ .State.Running }}" $DBNAME`
+RUNNING=`docker inspect --format="{{ .State.Running }}" $NAME`
 if [ "$RUNNING" == "false" ]; then
-    echo "Container $DBNAME is not running"
-    docker start $DBNAME
+    echo "Container $NAME is not running"
+    docker start $NAME
+else
+    echo "Container $NAME is still running, exiting"
+    exit 0
 fi
 
-docker exec -i $DBNAME sh -c "cd /code; bundle install"
+docker exec -i $NAME sh -c "cd /code; bundle install"
 
-docker exec -i $DBNAME sh -c "RACK_ENV=production ruby /code/lib/tradedata_crawler.rb >> /logs/tradedata_crawler.log 2>&1"
+docker exec -i $NAME sh -c "RACK_ENV=production ruby /code/lib/tradedata_crawler.rb >> /logs/tradedata_crawler.log 2>&1"
+
+docker stop $NAME
